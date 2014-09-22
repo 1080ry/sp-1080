9e2928bf639f8e717880a4bb23951629ac2db1a1
1080ry <yoshihisa_toyama@Yoshihisa-Toyama-no-MacBook-Air.local>
2014-09-22 23:44:22 +0900

    header

--- header.php
+++ header.php
@@ -17,11 +17,11 @@
     <link href='http://fonts.googleapis.com/css?family=Passion+One:700' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:600italic' rel='stylesheet' type='text/css'>
     <!-- font awesome -->
-    <link href="
-    <?php bloginfo('template_url'); ?>/css/font-awesome.min.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
+
     <!-- favicon -->
     <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/favicon.ico">
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+
     <?php wp_head()?>
   </head>
   <body>
@@ -56,6 +56,14 @@
             <a href=""><i class="fa fa-angle-right"></i> カテゴリ</a>
           </li>
         </ul>
+
+<form role="search" method="get" id="searchform" class="menu_search" action="http://portfolio.tank.jp/gogo/" >
+  <input type="text" value="" class="search_text" name="s" id="s" class="search_text" placeholder="キーワードを入力" />
+  <div class="box_search_btn">
+        <i class="fa fa-search"></i>
+    <input type="submit" id="searchbtn" value="検索" class="search_btn">
+  </div>
+</form>
       </nav>
       <!--TOPページ以外でパンくずリストを表示する-->
       <?php if ( !is_home() && !is_front_page() ) : ?>
@@ -70,7 +78,7 @@
       <header class="header">
         <h1 class="logo">
           <a href="<?php bloginfo('url'); ?>">
-            <img src="<?php bloginfo('template_url'); ?>/img/sp/logo.png" width="75" height="40" alt="1080ry.com">
+            <img src="<?php bloginfo('template_url'); ?>/img/logo.png" width="75" height="40" alt="ロゴ">
           </a>
         </h1>
         <div class="menu_btn"></div>
