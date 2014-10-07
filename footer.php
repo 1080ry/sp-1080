60c1e4204df06851137574570cfcfe6f019911d7
1080ry <yoshihisa_toyama@Yoshihisa-Toyama-no-MacBook-Air.local>
2014-10-07 14:02:54 +0900

    footer

--- footer.php
+++ footer.php
@@ -1,70 +1,48 @@
-      <footer id="footer">
-        <section class="footer_cat">
-          <h1>ブログ カテゴリー</h1>
-          <ul>
-            <li>
-              <a href="＃">カテゴリ</a>
-            </li>
-            <li>
-              <a href="＃">カテゴリ</a>
-            </li>
-            <li>
-              <a href="＃">カテゴリ</a>
-            </li>
-            <li>
-              <a href="＃">カテゴリ</a>
-            </li>
-            <li>
-              <a href="＃">カテゴリ</a>
-            </li>
-            <li class="last">
-              <a href="＃">カテゴリ</a>
-            </li>
-          </ul>
-        </section>
-        <!-- /footer_cat -->
-        <div class="ad">
-          <p>スポンサーリンク</p>
-<img src="http://placehold.it/300x250">
-        </div>
-        <nav class="footer_menu">
-          <ul>
-            <li>
-              <a href="#"><i class="fa fa-home"></i>HOME</a>
-            </li>
-            <li>
-              <a href="#">カテゴリ</a>
-            </li>
-            <li>
-              <a href="#">カテゴリ</a>
-            </li>
-            <li>
-              <a href="#">カテゴリ</a>
-            </li>
-            <li>
-              <a href="#">カテゴリ</a>
-            </li>
-            <li>
-              <a href="#">カテゴリ</a>
-            </li>
-            <li>
-              <a href="#">カテゴリ</a>
-            </li>
-            <li>
-              <a href="#">カテゴリ</a>
-            </li>
-          </ul>
-        </nav>
-        <!-- /footer_menu -->
-        <div class="copyright">
-          <small>©
-            <?php bloginfo('name'); ?>
-          </small>
-        </div>
-      </footer>
-    </div>
-    <script src="<?php bloginfo('template_url'); ?>/js/sp-1080.js"></script>
-    <!-- sns -->
+<footer id="footer">
+   <section class="footer_cat">
+    <h1>ブログ カテゴリー</h1>
+    <ul>
+     <li><a href="<?php bloginfo('url'); ?>/">カテゴリー</a></li>
+     <li><a href="<?php bloginfo('url'); ?>/">カテゴリー</a></li>
+     <li><a href="<?php bloginfo('url'); ?>/">カテゴリー</a></li>
+     <li><a href="<?php bloginfo('url'); ?>/">カテゴリー</a></li>
+     <li><a href="<?php bloginfo('url'); ?>/">カテゴリー</a></li>
+     <li><a href="<?php bloginfo('url'); ?>/">カテゴリー</a></li>
+     <li class="last"><a href="<?php bloginfo('url'); ?>/">カテゴリー</a></li>
+    </ul>
+   </section><!-- /footer_cat -->
+
+<nav class="footer_menu">
+  <ul>
+    <li><a href="<?php bloginfo('url'); ?>"><i class="fa fa-home"></i>HOME</a></li>
+    <li><a href="<?php bloginfo('url'); ?>/sitemap">サイトマップ</a></li>
+  </ul>
+</nav><!-- /footer_menu -->
+
+<div class="copyright">
+  <small>©<?php bloginfo('name'); ?></small>
+</div>
+
+<div class="pagetop">
+  <a href="#logo">
+    <div class="pagetop-arrow"></div>
+    <p>PAGE TOPへ</p>
+  </a>
+</div><!-- /page topへ -->
+
+<div class="pc-switch-footer">
+ <a href="<?php echo("http://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]); ?>?pc-switcher=1">PCサイト</a>
+</div>
+
+
+</footer>
+</div>
+
+<script src="<?php bloginfo('template_url'); ?>/js/jquery.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/sp-1080.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/smoothScroll.js"></script>
+
+<!-- sns -->
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
